8739b94969fd829e721b83c48560f4ea23619677
ttt1091 <tez1091@yahoo.co.jp>
2021-09-29 21:01:23 +0900

    change

--- jobster/register4work.php
+++ jobster/register4work.php
@@ -24,8 +24,12 @@ if($calendar_resources==28){
 } else {
   $calendar_weekly = 6;
 }
-
-echo $calendar_weekly;
+$view_day = 1;
+$myId = intval($_COOKIE['theChatYouID']);
+$select_job_user = $dbh->prepare("SELECT * FROM `jobster_user_settings` WHERE `manager_id` = :mid");
+$select_job_user->execute([
+  ':mid' => $myId
+]);
 
 ?>
 
@@ -60,29 +64,22 @@ echo $calendar_weekly;
       <div class="jobster-work-register-calendar-wrap">
         <div>2021/09</div>
         <div>
-<?php for ($calendar_weekly_count = 1; $calendar_weekly_count <= $calendar_weekly; $calendar_weekly_count++){ ?>
-  <?= $calendar_weekly_count ?>
-<?php } ?>
+<?php for($calendar_weekly_count = 1; $calendar_weekly_count <= $calendar_weekly; $calendar_weekly_count++){ ?>
           <div class="jobster-work-register-calendar-weekly">
-            <dl class="background-disable">
-              <dt>29日</dt>
-              <dd class="official-message"></dd>
-              <dd></dd>
-            </dl>
-            <dl class="background-disable">
-              <dt>30日</dt>
-              <dd class="official-message"></dd>
-              <dd></dd>
-            </dl>
-            <dl class="background-disable">
-              <dt>31日</dt>
-              <dd class="official-message"></dd>
-              <dd></dd>
-            </dl>
-            <dl>
-              <dt>1日</dt>
+  <?php for($colender_weekly_in_days_count=1;$colender_weekly_in_days_count<=7;$colender_weekly_in_days_count++){ ?>
+    <?php if($calendar_weekly_count==1){
+      if($colender_weekly_in_days_count<($calendar_settings+1)){ $day_disable = 'background-disable';$day_view_flag = 0; }
+      elseif($colender_weekly_in_days_count==1) { $day_disable = 'background-sunday';$day_view_flag = 1; }
+      elseif($colender_weekly_in_days_count==7) { $day_disable = 'background-saturday';$day_view_flag = 1; }
+      else { $day_disable = '';$day_view_flag = 1; }
+    ?>
+            <dl class="<?= $day_disable ?>">
+              <dt>
+                <?php if(($calendar_settings+1)<=$colender_weekly_in_days_count){ echo $view_day;$view_day++; } else { echo '　'; } ?>
+              </dt>
               <dd class="official-message"></dd>
               <dd>
+<?php if($day_view_flag==1){ ?>
                 <div>
                   <div>
                     <label for="">
@@ -92,23 +89,34 @@ echo $calendar_weekly;
                   </div>
                   <div>
                     <select name="" id="">
-<?php for ($count = 9; $count < 23; $count++){ ?>
-                      <option value="<?= $count ?>"><?= $count ?></option>
+                      <option value="null">-</option>
+<?php for ($count_start = 9; $count_start < 23; $count_start++){ ?>
+                      <option value="<?= $count_start ?>"><?= $count_start ?></option>
 <?php } ?>
                     </select>
                     <select name="" id="">
-<?php for ($count = 9; $count < 23; $count++){ ?>
-                      <option value="<?= $count ?>"><?= $count ?></option>
+                      <option value="null">-</option>
+<?php for ($count_end = 9; $count_end < 23; $count_end++){ ?>
+                      <option value="<?= $count_end ?>"><?= $count_end ?></option>
 <?php } ?>
                     </select>
                   </div>
                 </div>
+<?php } ?>
               </dd>
             </dl>
-            <dl>
-              <dt>2日</dt>
+    <?php } else {
+      if($colender_weekly_in_days_count==1) { $day_disable = 'background-sunday';$day_view_flag = 1; }
+      elseif($colender_weekly_in_days_count==7) { $day_disable = 'background-saturday';$day_view_flag = 1; }
+      else { $day_disable = ''; }
+    ?>
+            <dl class="<?= $day_disable ?>">
+              <dt>
+                <?php echo $view_day;$view_day++; ?>
+              </dt>
               <dd class="official-message"></dd>
               <dd>
+<?php if($day_view_flag==1){ ?>
                 <div>
                   <div>
                     <label for="">
@@ -118,72 +126,26 @@ echo $calendar_weekly;
                   </div>
                   <div>
                     <select name="" id="">
+                      <option value="null">-</option>
 <?php for ($count = 9; $count < 23; $count++){ ?>
                       <option value="<?= $count ?>"><?= $count ?></option>
 <?php } ?>
                     </select>
                     <select name="" id="">
+                      <option value="null">-</option>
 <?php for ($count = 9; $count < 23; $count++){ ?>
                       <option value="<?= $count ?>"><?= $count ?></option>
 <?php } ?>
                     </select>
                   </div>
                 </div>
+<?php } ?>
               </dd>
             </dl>
-            <dl>
-              <dt>3日</dt>
-              <dd class="official-message"></dd>
-              <dd>
-                <div>
-                  <div>
-                    <label for="">
-                      <input type="checkbox" name="" id="">
-                      Default
-                    </label>
-                  </div>
-                  <div>
-                    <select name="" id="">
-<?php for ($count = 9; $count < 23; $count++){ ?>
-                      <option value="<?= $count ?>"><?= $count ?></option>
-<?php } ?>
-                    </select>
-                    <select name="" id="">
-<?php for ($count = 9; $count < 23; $count++){ ?>
-                      <option value="<?= $count ?>"><?= $count ?></option>
-<?php } ?>
-                    </select>
-                  </div>
-                </div>
-              </dd>
-            </dl>
-            <dl>
-              <dt>4日</dt>
-              <dd class="official-message"></dd>
-              <dd>
-                <div>
-                  <div>
-                    <label for="">
-                      <input type="checkbox" name="" id="">
-                      Default
-                    </label>
-                  </div>
-                  <div>
-                    <select name="" id="">
-<?php for ($count = 9; $count < 23; $count++){ ?>
-                      <option value="<?= $count ?>"><?= $count ?></option>
-<?php } ?>
-                    </select>
-                    <select name="" id="">
-<?php for ($count = 9; $count < 23; $count++){ ?>
-                      <option value="<?= $count ?>"><?= $count ?></option>
-<?php } ?>
-                    </select>
-                  </div>
-                </div>
-              </dd>
-            </dl>
+    <?php } ?>
+  <?php } ?>
           </div>
+<?php } ?>
 
         </div>
       </div>
